690582f9ca8a6a6b6a4054e1896850f4c6863d0b
Ranjeet_Deshmukh <ranjeet.deshmukh2699@gmail.com>
2026-07-30 00:28:39 +0530

    SD-003 Implemented Login Page object

---
+++ pages/login_page.py
@@ -0,0 +1,25 @@
+from playwright.sync_api import Page
+import config
+class LoginPage:
+    def __init__(self,page:Page):
+        self.page = page
+        self.username_field = page.get_by_role("textbox",name="user-name")
+        self.password_field = page.get_by_role("textbox",name="password")
+        self.login_button = page.locator("#login-button")
+
+    def open(self):
+        self.page.goto(config.BASE_URL)
+
+    def enter_username(self,username):
+        self.username_field.fill(username)
+
+    def enter_password(self,password):
+        self.password_field.fill(password)
+
+    def click_login(self):
+        self.login_button.click()
+
+    def login(self,username,password):
+        self.enter_username(username)
+        self.enter_password(password)
+        self.click_login()
---
+++ test_data/users.py
@@ -0,0 +1,1 @@
+VALID_USER = {"username":"standard_user", "password":"secret_sauce"}
