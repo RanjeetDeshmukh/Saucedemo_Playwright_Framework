079aec0db7f1f17380f0c021b5ac5a978fda8f89
Ranjeet_Deshmukh <ranjeet.deshmukh2699@gmail.com>
2026-07-30 17:58:56 +0530

    SD-007 Test invalid user login

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import expect
-from test_data.users import VALID_USER
+from test_data.users import VALID_USER, INVALID_USER
 import re
+import config
 from constants.routes import INVENTORY_PAGE
 
 def test_valid_user_can_login(login_page,inventory_page):
@@ -10,3 +11,11 @@ def test_valid_user_can_login(login_page,inventory_page):
     #assert user logged successfully & navigated to inventory page
     expect(inventory_page.page).to_have_url(re.compile(fr"{INVENTORY_PAGE}"))
     expect(inventory_page.header).to_have_text("Products")
+
+def test_invalid_user_cant_login(login_page):
+    login_page.open()
+    login_page.login(INVALID_USER["username"],INVALID_USER["password"])
+
+    #assert user does not login & correct error message is displayed
+    expect(login_page.error_msg_container).to_contain_text("Username and password do not match")
+    expect(login_page.page).to_have_url(config.BASE_URL)
